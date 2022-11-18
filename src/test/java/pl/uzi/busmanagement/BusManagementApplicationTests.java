b4fd40a428e42b8f74395a7d0888a5e38b220bbb
Patryk Uzarowski <patrykw595@gmail.com>
2022-11-18 14:47:25 +0100

    [MS-1]: Initial commit

---
+++ src/test/java/pl/uzi/busmanagement/BusManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uzi.busmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
